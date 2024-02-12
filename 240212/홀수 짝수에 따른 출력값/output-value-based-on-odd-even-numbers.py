206e615b1238c53263a872d5a9d50701f36157ed
Dawon Park <60343930+dawonparkk@users.noreply.github.com>
2024-02-12 23:50:20 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240212/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -0,0 +1,13 @@
+def num(n):
+    add = 0
+    if n%2==1:
+        for i in range(1,n+1,2):
+            add+=i
+        return add
+    else:
+        for i in range(2,n+1,2):
+            add+=i
+        return add
+
+#n = int(input())
+print(num(8))
